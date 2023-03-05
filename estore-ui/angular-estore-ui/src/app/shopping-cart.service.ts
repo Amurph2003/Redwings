c8f52f5a6e8379ad763efda77e92d534e80b255a
Alex <amm1205@rit.edu>
2023-03-04 19:13:38 -0500

    github pages - service addresses

--- estore-ui/angular-estore-ui/src/app/shopping-cart.service.ts
+++ estore-ui/angular-estore-ui/src/app/shopping-cart.service.ts
@@ -12,8 +12,8 @@ import { User } from './user';
 })
 export class ShoppingCartService {
 
-  private shoppingCartsUrl = 'https://140.84.180.148:8080/shopping-carts';
-  private jerseysUrl = 'https://140.84.180.148:8080/products';
+  private shoppingCartsUrl = 'http://140.84.180.148:8080/shopping-carts';
+  private jerseysUrl = 'http://140.84.180.148:8080/products';
 
 
   httpOptions = {
